3c5d6b045cf042f672f4ed84c1809a0fc45ea2f9
ddll <107200036+1760731085@users.noreply.github.com>
2024-11-07 10:48:26 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -8,17 +8,18 @@ from libs.source import ZWYT
 def main(*args, **kwargs):
     # 遍历 info 信息，获取每个用户的昵称、预约座位号、用户名、密码、时间段、推送token（推送可以为空）
     for stu in infos:
-        try:
-            # 初初始化类示例，传入昵称、用户名、密码、时间段、推送token（推送可以为空）
-            yy = ZWYT(stu['name'], stu['sno'], stu['pwd'], stu['periods'], stu['pushplus'])
+        for devName in stu['devName']:  # 遍历多个预约位置
+            try:
+                # 初始化类示例，传入昵称、用户名、密码、时间段、推送token（推送可以为空）
+                yy = ZWYT(stu['name'], stu['sno'], stu['pwd'], stu['periods'], stu['pushplus'])
 
-            # 调用签到函数进行签到，传入预约座位号
-            yy.sign(stu['devName'])
-        except Exception as e:
-            print(e)
-            if stu['pushplus']:
-                yy.pushplus(f"{stu['name']} {stu['devName']} 签到失败", e)
-            continue
+                # 调用签到函数进行签到，传入预约座位号
+                yy.sign(devName)  # 使用 devName 变量进行签到
+            except Exception as e:
+                print(e)
+                if stu['pushplus']:
+                    yy.pushplus(f"{stu['name']} {devName} 签到失败", e)
+                continue
 
 
 if __name__ == '__main__':
