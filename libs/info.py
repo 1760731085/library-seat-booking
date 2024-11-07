0a718ca29eac0899c427294a95288900a5f94ee3
ddll <107200036+1760731085@users.noreply.github.com>
2024-11-07 10:42:53 +0800

    Update info.py

--- libs/info.py
+++ libs/info.py
@@ -2,15 +2,15 @@ import os
 
 infos = [
     {
-        'sno': os.getenv('USER_SNO'),         # 从环境变量获取学号
-        'pwd': os.getenv('USER_PWD'),         # 从环境变量获取密码
-        'devName': os.getenv('USER_DEVNAME'), # 从环境变量获取预约的座位号
-        'name': os.getenv('USER_NAME'),       # 从环境变量获取用户名
+        'sno': os.getenv('USER_SNO'),
+        'pwd': os.getenv('USER_PWD'),
+        'devName': ['103-90', '103-80', '103-70', '103-89', '103-79'],  # 多个预约位置
+        'name': os.getenv('USER_NAME'),
         'periods': (
             ('9:30:00', '13:30:00'),
             ('14:30:00', '18:00:00'),
             ('18:30:00', '22:00:00')
         ),
-        'pushplus': os.getenv('USER_PUSHPLUS')  # 从环境变量获取 pushplus 的 token（用于推送消息到微信，可为空）
+        'pushplus': os.getenv('USER_PUSHPLUS')
     }
 ]
